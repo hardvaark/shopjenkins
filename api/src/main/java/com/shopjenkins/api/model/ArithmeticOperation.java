f0f99fa58f794c3aceb3a531d7e4e8fac5a2123f
laurezi <ezih.laurent@gmail.com>
2024-05-27 01:48:14 +0000

    api update v1 modify for test +2

--- api/src/main/java/com/shopjenkins/api/model/ArithmeticOperation.java
+++ api/src/main/java/com/shopjenkins/api/model/ArithmeticOperation.java
@@ -10,7 +10,7 @@ public class ArithmeticOperation {
  }
 
  public double add() {
-     return term1 - term2;
+     return term1 + term2;
  }
 
  public double subtract() {
@@ -18,7 +18,7 @@ public class ArithmeticOperation {
  }
 
  public double multiply() {
-     return term1 + term2;
+     return term1 * term2;
  }
 
  public double divide() {
